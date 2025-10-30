895960632c1e68cc20ac74ef3bfc282f3ef93e30
悟空 <rainchan365@163.com>
2025-10-30 18:39:19 +0800

    Add Tauri scripts for IPC, core, and initialization

--- examples/greet.rs
+++ examples/greet.rs
@@ -1,56 +1,40 @@
 use gpui::{
-    div, px, rgb, size, App, AppContext, Application, Bounds, Context, IntoElement, ParentElement,
-    Render, SharedString, Styled, Window, WindowBounds, WindowOptions,
+    div, px, App, AppContext, Application, Context, Entity, IntoElement, ParentElement, Render,
+    Styled, Window, WindowOptions,
 };
+use gpui_component::webview::WebView;
+use gpui_component::{v_flex, wry, ActiveTheme};
+use wry::WebViewBuilder;
 
 fn main() {
     Application::new().run(|cx: &mut App| {
-        let bounds = Bounds::centered(None, size(px(500.), px(500.0)), cx);
-        cx.open_window(
-            WindowOptions {
-                window_bounds: Some(WindowBounds::Windowed(bounds)),
-                ..Default::default()
-            },
-            |_, cx| {
-                cx.new(|_| HelloWorld {
-                    text: "World".into(),
-                })
-            },
-        )
+        cx.activate(true);
+
+        cx.open_window(WindowOptions::default(), |window, cx| {
+            GreetView::view(window, cx)
+        })
         .unwrap();
     });
 }
 
-struct HelloWorld {
-    text: SharedString,
+struct GreetView {
+    webview: Entity<WebView>,
 }
 
-impl Render for HelloWorld {
-    fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
-        div()
-            .flex()
-            .flex_col()
-            .gap_3()
-            .bg(rgb(0x505050))
-            .size(px(500.0))
-            .justify_center()
-            .items_center()
-            .shadow_lg()
-            .border_1()
-            .border_color(rgb(0x0000ff))
-            .text_xl()
-            .text_color(rgb(0xffffff))
-            .child(format!("Hello, {}!", &self.text))
-            .child(
-                div()
-                    .flex()
-                    .gap_2()
-                    .child(div().size_8().bg(gpui::red()))
-                    .child(div().size_8().bg(gpui::green()))
-                    .child(div().size_8().bg(gpui::blue()))
-                    .child(div().size_8().bg(gpui::yellow()))
-                    .child(div().size_8().bg(gpui::black()))
-                    .child(div().size_8().bg(gpui::white())),
-            )
+impl GreetView {
+    pub fn view(window: &mut gpui::Window, app: &mut App) -> Entity<WebView> {
+        let webview = app.new(|cx: &mut Context<WebView>| {
+            let webview = {
+                use raw_window_handle::HasWindowHandle;
+
+                let window_handle = window.window_handle().expect("No window handle");
+                let builder = WebViewBuilder::new().with_url("https://v2.tauri.org.cn");
+                let webview = builder.build_as_child(&window_handle).unwrap();
+                webview
+            };
+            WebView::new(webview, window, cx)
+        });
+
+        webview
     }
 }
